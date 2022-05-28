f288500759c33e7c57f018f79c3501c6d7f0c14f
aray.baimolda <baimolda.2001@gmail.com>
2022-05-28 06:18:24 +0600

    initial commit created project using start.spring.io

---
+++ src/test/java/guru/springframework/springJokesApp/SpringJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springJokesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
